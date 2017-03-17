fa1ac5537f481e622f54e1c94a2ddbc275fff30a
Hartley Brody <hartley.brody@gmail.com>
2017-03-17 13:48:26 +1000

    start setting up the HTML template structure

--- dash/views.py
+++ dash/views.py
@@ -2,16 +2,13 @@ from flask import Blueprint, render_template, request, redirect, session, flash,
 
 from dash.utils import login_required
 
-dash = Blueprint('dash', __name__)
+dash = Blueprint('dash', __name__, template_folder="dash")
 
 @dash.route('/dashboard/', methods=['GET'])
 @login_required
 def dashboard():
-    if not request.user.access_token:
 
-        return render_template("dashboard/welcome.html")
+    if False:
+        return redirect(url_for(".some_other_view_function_name"))
 
-    if request.user.stats:
-        return redirect(url_for(".dashboard_leaderboard"))
-
-    return redirect(url_for(".dashboard_form"))
+    return render_template("root.html")
