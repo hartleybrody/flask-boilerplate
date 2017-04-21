0c3d0c56e7d0f210ba994df4563b47005d0e84c8
Hartley Brody <hartley.brody@gmail.com>
2017-04-21 16:39:39 -0400

    add a quick managment command for creating admin users

--- manage.py
+++ manage.py
@@ -25,5 +25,32 @@ def job(name):
     print "Running custom job: {}".format(name)
 
 
+@manager.option("-e", "--email", dest="email", default=None)
+@manager.option("-p", "--pass", dest="password", default=None)
+def admin(email, password):
+    if not email or not password:
+        return "Can't setup admin without email and password"
+
+    u = User(email=email, is_admin=True)
+    u.set_password(password)
+
+    try:
+        db.session.add(u)
+        db.session.commit()
+
+        return "Successfully added {} with that password".format(email)
+    except IntegrityError:
+        db.session.rollback()
+        print "Admin with that email already exists, updating their password"
+
+        u = User.query.filter_by(email=email).one()
+        u.set_password(password)
+
+        db.session.add(u)
+        db.session.commit()
+
+        return "Successfully updated the admin password for {}".format(email)
+
+
 if __name__ == '__main__':
     manager.run()
