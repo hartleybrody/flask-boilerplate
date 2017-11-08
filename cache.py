0639793824e273551ad26bb93b6e259857c559d1
Hartley Brody <hartley.brody@gmail.com>
2017-11-08 18:06:21 -0500

    refactor the redis connection pieces so we can import the redis connection (also stop using redis for every variable name and have import side effects)

--- cache.py
+++ cache.py
@@ -5,6 +5,9 @@ import redis
 
 DEFAULT_EXPIRES = 60 * 15  # number of seconds before we auto-expire keys
 
+u = urlparse(os.environ["REDIS_URL"])
+redis_connection = redis.StrictRedis(host=u.hostname, port=u.port, password=u.password, db=0)
+
 
 class RedisCache(object):
     """docstring for RedisCache"""
@@ -13,31 +16,29 @@ class RedisCache(object):
         if not os.environ["REDIS_URL"]:
             raise Exception("Can't use redis without specifying valid `REDIS_URL` env variable.")
 
-        u = urlparse(os.environ["REDIS_URL"])
-        self.redis = redis.StrictRedis(host=u.hostname, port=u.port, password=u.password, db=0)
+        self.connection = redis_connection
 
     def set(self, k, v, expires=DEFAULT_EXPIRES):
-        return self.redis.set(k, v, ex=int(expires))
+        return self.connection.set(k, v, ex=int(expires))
 
     def get(self, k, default=None):
-        return self.redis.get(k) or default
+        return self.connection.get(k) or default
 
     def delete(self, k):
-        return self.redis.delete(k)
+        return self.connection.delete(k)
 
     def scan_iter(self, k):
-        return self.redis.scan_iter(k)
+        return self.connection.scan_iter(k)
 
     def delete_pattern(self, pattern):
-        for key in redis.scan_iter(pattern):
-            redis.delete(key)
+        for key in self.connection.scan_iter(pattern):
+            self.connection.delete(key)
+
+# instantiate it so we can just import the name 'redis' and use it anywhere
+redis = RedisCache()
 
 if __name__ == '__main__':
     # test connection
-    r = RedisCache()
-    r.set('hartley', 'rulez')
-    print r.get('hartley')
-
-else:
-    # instantiate it so we can just import the name 'redis' and use it anywhere
-    redis = RedisCache()
+    redis.set('test:hartley', 'rulez')
+    print redis.get('test:hartley')
+    # redis.delete_pattern("test:*")
