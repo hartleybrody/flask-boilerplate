9118e90ac3aa47370228080c7b84fad7c4c99ad2
Hartley Brody <hartley.brody@gmail.com>
2018-02-20 12:39:53 -0500

    add a cache-buster query argument to static file, based on most recent file update

--- app.py
+++ app.py
@@ -26,6 +26,23 @@ if os.environ.get("SENTRY_DSN"):
 app.register_blueprint(web_blueprint)
 app.register_blueprint(dash_blueprint)
 
+# add a timestamp-based cache buster to static files
+static_last_update = 0
+for directory in os.walk("static/"):
+    file_names = directory[2]
+    for file_name in file_names:
+        if file_name.startswith("."):
+            continue  # ignore config files
+        ext = file_name.split(".")[-1]
+        if ext not in ["css", "js"]:
+            continue  # skip fonts and images
+        file_path = os.path.join(directory[0], file_name)
+        updated_at = os.path.getmtime(file_path)
+        if updated_at > static_last_update:
+            static_last_update = updated_at
+app.config['static_last_update'] = str(int(static_last_update))[-6:]
+print app.config['static_last_update']
+
 @app.errorhandler(404)
 def not_found(e):
     return render_template("404.html"), 404
