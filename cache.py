ed5e350e1c39f0a26cec4c400a7c08f05e691b41
Hartley Brody <hartley.brody@gmail.com>
2021-07-01 10:26:05 -0400

    add default key:prefix and allow set() with no expiration

--- cache.py
+++ cache.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 import redis
 
 DEFAULT_EXPIRES = 60 * 15  # number of seconds before we auto-expire keys
+KEY_PREFIX = "{{APP_SLUG}}"
 
 u = urlparse(os.environ["REDIS_URL"])
 redis_connection = redis.StrictRedis(host=u.hostname, port=u.port, password=u.password, db=0)
@@ -18,27 +19,44 @@ class RedisCache(object):
 
         self.connection = redis_connection
 
+    def _build_key(self, k):
+        if type(k) == bytes:
+            k = k.decode()  # convert bytes to str
+        if k.startswith(f"{KEY_PREFIX}:"):
+            return k
+        return f"{KEY_PREFIX}:{k}"
+
     def set(self, k, v, expires=DEFAULT_EXPIRES):
-        return self.connection.set(k, v, ex=int(expires))
+        k = self._build_key(k)
+        if expires:
+            return self.connection.set(k, v, ex=int(expires))
+        return self.connection.set(k, v)
 
     def get(self, k, default=None):
+        k = self._build_key(k)
         return self.connection.get(k) or default
 
     def delete(self, k):
+        k = self._build_key(k)
         return self.connection.delete(k)
 
     def scan_iter(self, k):
+        k = self._build_key(k)
         return self.connection.scan_iter(k)
 
     def delete_pattern(self, pattern):
-        for key in self.connection.scan_iter(pattern):
-            self.connection.delete(key)
+        for key in self.scan_iter(pattern):
+            self.delete(key)
 
 # instantiate it so we can just import the name 'redis' and use it anywhere
 redis = RedisCache()
 
 if __name__ == '__main__':
     # test connection
-    redis.set('test:hartley', 'rulez')
-    print(redis.get('test:hartley'))
-    # redis.delete_pattern("test:*")
+    redis.set('test:foo', 'bar')
+    redis.set('test:hello', 'world')
+    for k in redis.scan_iter('test:*'):
+        v = redis.get(k)
+        print(f"{k} => {v}")
+
+    redis.delete_pattern("test:*")
